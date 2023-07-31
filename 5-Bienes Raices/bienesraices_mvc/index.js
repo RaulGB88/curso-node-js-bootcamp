01dd685d331388067c3c0940270e9f04a99e3f35
RaulGB88 <rgranel@viewnext.com>
2023-07-31 15:15:54 +0200

    v1.0.0.1
    Init curs

---
+++ 5-Bienes Raices/bienesraices_mvc/index.js
@@ -0,0 +1,19 @@
+// 1- Import Express dependency
+const express = require('express');
+
+// 2- Create App
+const app = express();
+
+// 3- Define Port adn Run Project
+const port = 3000
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+})
+
+// 4- Routing
+app.get('/', function(req, res) {
+    res.json({msg: 'Hola Mundo Json'})
+});
+app.get('/hello', function(req, res) {
+    res.send('Hola Mundo en express')
+});
